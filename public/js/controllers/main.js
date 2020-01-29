3b28aedaf0effe9f1688cf5bd904c5896c15bc3a
sanjay16aug <60364150+sanjay16aug@users.noreply.github.com>
2020-01-29 22:11:50 +0000

    code update
    update code

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -47,4 +47,94 @@ angular.module('todoController', [])
 					$scope.todos = data; // assign our new list of todos
 				});
 		};
+
+		$scope.mydraw = function(data) {
+			drawGraph(data);
+		};
+		
+
+
+		function drawGraph(data) {
+			var btn = document.getElementById("run"),
+				cd = document.getElementById("code"),
+				chart;
+
+			(btn.onclick = function () {
+				var code = cd.value;
+
+				if (chart) {
+					chart.clean();
+				}
+				if(data == undefined){
+					chart = flowchart.parse(code);
+				}else{
+					chart = flowchart.parse(data);
+				}
+				
+				chart.drawSVG('canvas', {
+					'line-width': 3,
+					'maxWidth': 100,
+					'text-margin': 10,
+					'font-size': 14,
+					'font': 'normal',
+					'font-family': 'Helvetica',
+					'font-weight': 'normal',
+					'font-color': 'black',
+					'line-color': 'black',
+					'element-color': 'black',
+					'fill': 'white',
+					'yes-text': 'yes',
+					'no-text': 'no',
+					'arrow-end': 'block',
+					'scale': 1,
+					'symbols': {
+						'start': {
+							'font-color': 'red',
+							'element-color': 'green',
+							'fill': 'yellow'
+						},
+						'end': {
+							'class': 'end-element'
+						}
+					},
+					'flowstate': {
+						'past': {
+							'fill': '#CCCCCC',
+							'font-size': 12
+						},
+						'current': {
+							'fill': 'yellow',
+							'font-color': 'red',
+							'font-weight': 'bold'
+						},
+						'future': {
+							'fill': '#FFFF99'
+						},
+						'request': {
+							'fill': 'blue'
+						},
+						'invalid': {
+							'fill': '#444444'
+						},
+						'approved': {
+							'fill': '#58C4A3',
+							'font-size': 12,
+							'yes-text': 'APPROVED',
+							'no-text': 'n/a'
+						},
+						'rejected': {
+							'fill': '#C45879',
+							'font-size': 12,
+							'yes-text': 'n/a',
+							'no-text': 'REJECTED'
+						}
+					}
+				});
+
+				$('[id^=sub1]').click(function () {
+					alert('info here');
+				});
+			})();
+		}
+
 	}]);
