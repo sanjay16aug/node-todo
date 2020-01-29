fcc4764c9d60e2cfe8424db1543b25f337d422b2
sanjay16aug <60364150+sanjay16aug@users.noreply.github.com>
2020-01-29 16:30:39 +0000

    Update database.js
    db added

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
-    localUrl: 'mongodb://localhost/meanstacktutorials'
+    remoteUrl : 'mongodb://flagsdb:V4dfZ7HQcP7ttEjjgrieDzNwiEJkg5R3Vhfjo17CFgIJozPBrBeBOzXybOSof66TeiX62I7JkDkulM6g5X631g==@flagsdb.documents.azure.com:10255/?ssl=true&replicaSet=globaldb',
+    localUrl: 'mongodb://flagsdb:V4dfZ7HQcP7ttEjjgrieDzNwiEJkg5R3Vhfjo17CFgIJozPBrBeBOzXybOSof66TeiX62I7JkDkulM6g5X631g==@flagsdb.documents.azure.com:10255/?ssl=true&replicaSet=globaldb'
 };
