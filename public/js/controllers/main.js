e66e53b070d070700a8dec7458f28d14b28083bb
sanjay16aug <60364150+sanjay16aug@users.noreply.github.com>
2020-02-04 17:16:22 +0000

    Update main.js
    update the js

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -11,7 +11,6 @@ angular.module('todoController', [])
 		Todos.get()
 			.success(function(data) {
 				$scope.todos = data;
-				//drawGraph(todos[0].text);
 				$scope.loading = false;
 			});
 
@@ -48,99 +47,4 @@ angular.module('todoController', [])
 					$scope.todos = data; // assign our new list of todos
 				});
 		};
-
-		$scope.mydraw = function(data) {
-			drawGraph(data);
-		};
-
-		$scope.drawGraphData = function() {
-			console.log($scope.todos);
-			drawGraph($scope.todos[0].text);
-		};
-		
-
-
-		function drawGraph(data) {
-			var btn = document.getElementById("run"),
-				cd = document.getElementById("code"),
-				chart;
-
-			(btn.onclick = function () {
-				var code = cd.value;
-
-				if (chart) {
-					chart.clean();
-				}
-				if(data == undefined){
-					chart = flowchart.parse(code);
-				}else{
-					chart = flowchart.parse(data);
-				}
-				
-				chart.drawSVG('canvas', {
-					'line-width': 3,
-					'maxWidth': 100,
-					'text-margin': 10,
-					'font-size': 14,
-					'font': 'normal',
-					'font-family': 'Helvetica',
-					'font-weight': 'normal',
-					'font-color': 'black',
-					'line-color': 'black',
-					'element-color': 'black',
-					'fill': 'white',
-					'yes-text': 'yes',
-					'no-text': 'no',
-					'arrow-end': 'block',
-					'scale': 1,
-					'symbols': {
-						'start': {
-							'font-color': 'red',
-							'element-color': 'green',
-							'fill': 'yellow'
-						},
-						'end': {
-							'class': 'end-element'
-						}
-					},
-					'flowstate': {
-						'past': {
-							'fill': '#CCCCCC',
-							'font-size': 12
-						},
-						'current': {
-							'fill': 'yellow',
-							'font-color': 'red',
-							'font-weight': 'bold'
-						},
-						'future': {
-							'fill': '#FFFF99'
-						},
-						'request': {
-							'fill': 'blue'
-						},
-						'invalid': {
-							'fill': '#444444'
-						},
-						'approved': {
-							'fill': '#58C4A3',
-							'font-size': 12,
-							'yes-text': 'APPROVED',
-							'no-text': 'n/a'
-						},
-						'rejected': {
-							'fill': '#C45879',
-							'font-size': 12,
-							'yes-text': 'n/a',
-							'no-text': 'REJECTED'
-						}
-					}
-				});
-
-				$('[id^=sub1]').click(function () {
-					alert('info here');
-				});
-			})();
-		}
-
 	}]);
